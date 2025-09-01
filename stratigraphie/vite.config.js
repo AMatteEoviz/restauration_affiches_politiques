96ea375d58c063de5dfc20e963f157a9d3ff8af5
amatte1 <amatte@etu.univ-paris1.fr>
2025-09-01 10:33:25 +0200

    prep deployement

--- stratigraphie/vite.config.js
+++ stratigraphie/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/restauration_affiches_politiques/',
   plugins: [
     vue(),
     vueDevTools(),
